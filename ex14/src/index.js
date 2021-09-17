b21d25d000501ffb1f51f1cf0422110085fc1a79
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-17 22:27:36 +0200

    Adding word blanks

---
+++ ex14/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = "I " + myVerb + " so " + myAdverb + ", that the " + myAdjective + " " + myNoun + " couldn't catch me" + ".";
+    // only change code above this line
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
