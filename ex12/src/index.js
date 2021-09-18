be380996ebb467f5f5648849ef9aee9a8e96f362
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-18 10:44:46 +0200

    Added the proper initialization

--- ex12/src/index.js
+++ ex12/src/index.js
@@ -1,6 +1,6 @@
 function main() {
     // only change code below this line
-    lastName = "Tesla";
+    var lastName = "Tesla";
     var lastLetterOfLastName = lastName[lastName.length - 1];
     // only change code above this line
     return lastLetterOfLastName;
